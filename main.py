33fea27b8f659ebbae0a346b1b2284be40364e50
Z在此 <8880845+z-here@user.noreply.gitee.com>
2024-09-25 23:33:01 +0800

    delete notes

--- main.py
+++ main.py
@@ -11,28 +11,6 @@ import threading
 import keyboard
 
 
-# file = 'D:\\tmp\\test.txt\0D:\\tmp\\股票数据.xlsx\0\0'
-# data = file.encode("U16")[2:]
-#
-# win32clipboard.OpenClipboard()
-# try:
-#     win32clipboard.EmptyClipboard()
-#     win32clipboard.SetClipboardData(
-#         win32clipboard.CF_HDROP, bytes(pDropFiles)+data)
-# finally:
-#     win32clipboard.CloseClipboard()
-
-
-# formats = auto.GetClipboardFormats()
-# for k, v in formats.items():
-#     if k == auto.ClipboardFormat.CF_UNICODETEXT:
-#         print("文本格式：", auto.GetClipboardText())
-#     elif k == auto.ClipboardFormat.CF_HTML:
-#         htmlText = auto.GetClipboardHtml()
-#         print("富文本格式：", htmlText)
-#     elif k == auto.ClipboardFormat.CF_BITMAP:
-#         bmp = auto.GetClipboardBitmap()
-#         print("位图：", bmp)
 def thread_it(func, *args, daemon: bool = True):
     t = threading.Thread(target=func, args=args)
     t.daemon = daemon
@@ -57,19 +35,16 @@ imagesPathList = [os.path.join(imageFolderPath, i) for i in os.listdir(imageFold
                   os.path.splitext(i)[1].lower() in usualImageFormat]
 control = pynput.keyboard.Controller()
 
-# print(imagesPathList)
 
 imagesPathConvertedList = []
 for i in imagesPathList:
     if os.path.splitext(i)[1].lower() != '.gif':
-        # print(os.path.splitext(i))
         imageConvertedPath = f'{os.path.splitext(i)[0]}_converted.gif'
         os.system(f'ffmpeg -i {i} {imageConvertedPath}')
         os.remove(i)
         imagesPathConvertedList.append(imageConvertedPath)
     else:
         imagesPathConvertedList.append(i)
-# print(imagesPathConvertedList)
 for imageConvertedPath in imagesPathConvertedList:
     win32clipboard.OpenClipboard()
     win32clipboard.EmptyClipboard()
@@ -91,7 +66,3 @@ for imageConvertedPath in imagesPathConvertedList:
     time.sleep(0.5)
     control.press(pynput.keyboard.Key.enter)
 
-# cancelButton=getFilesDialogWindow.ButtonControl(Name='取消',ClassName='Button',depth=1)
-# # cancelButton.Click(simulateMove=True)
-# addressEdit = cancelButton.GetNextSiblingControl().GetNextSiblingControl()
-# addressEdit.Click()
